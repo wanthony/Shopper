99d3e73e11a303df087297aee184846adab64b9b
Anthony White <technology@tlsgltd.com>
2011-12-20 16:30:53 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopper::Application.config.secret_token = '921e3dac902ac0063c6fda3413c38a46f8ec940f5ae44efa9fabdd9835f40f66e033e654ace57ff4f7bfb6297a147f7e7921beb706ffda90fc52d8e2fbb39066'
